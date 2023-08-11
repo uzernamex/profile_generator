c0f2f5c4b831292e49db3682c3bc1cae093be302
Ayesha Cumberbatch <ayesha.416x@gmail.com>
2023-08-11 18:46:21 -0400

    added questions to survey

--- survey.js
+++ survey.js
@@ -5,8 +5,18 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question('What do you think of Node.js? ', (answer) => {
-  console.log(`Thank you for your valuable feedback: ${answer}`);
+rl.question('What\'s your name? (Nicknames are also acceptable'), (answer1) => {
+  rl.question('What\'s an activity you enjoy doing? '), (answer2) => {
+    console.log(Thank you for ${answer1} your valuable feedback: ${answer2});
+  }
+    /*
+What do you listen to while doing that?
+Which meal is your favourite (eg: dinner, brunch, etc.)
+What's your favourite thing to eat for that meal?
+Which sport is your absolute favourite?
+What is your superpower? In a few words, tell us what you are amazing at!
+*/
+  
+rl.close();
 
-  rl.close();
 });
